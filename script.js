55e9cf3b526ed98f050b2f6be5a0c238ee3b4e39
Syarif <syrfnr@gmail.com>
2026-01-04 12:19:23 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openInvitation() {
-  document.getElementById("cover").style.display = "none";
-  document.getElementById("content").classList.remove("hidden");
-
-  const music = document.getElementById("bgm");
-  music.play().catch(() => {});
-}
